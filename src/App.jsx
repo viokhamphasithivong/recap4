3a2438e755c3dec35630ad4620f69c8fdbdf2339
Saluomvienb <khamphasithivong.s@googlemail.com>
2026-03-07 19:45:03 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { initialColors } from "./lib/colors";
-import Color from "./Components/Color/Color";
-import "./App.css";
+import { initialColors } from "./lib/colors"; //getting array of colors
+import Color from "./Components/Color/Color"; //gettiing component for card
+import "./App.css"; //getting style 
 
 function App() {
   return (
     <>
       <h1>Theme Creator</h1>
-      // display colors here
+      {/* mapping through array of colors (schleife) */}
+      {initialColors.map(color => (
+        
+        /* passing color object into Color component */
+        <Color color={color} key={color.id}/>
+      ))}
+      
     </>
   );
 }
--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,5 +1,23 @@
 import "./Color.css";
 
 export default function Color({ color }) {
-  return null;
+
+
+  return (
+    <div className="color-card" style={{backgroundColor: color.hex, color: color.contrastText}}>
+
+      <p className="color-card-headline">{color.hex}</p>
+      <p className="color-card-role">Role: {color.role}</p>
+      <p >Contrast: <span className="color-card-contrast">{color.contrastText}</span></p>
+
+    </div>
+  )
+
 }
+
+
+
+/* hex value of the color 
+the role of the color
+the color itself in the background color of the element
+the font in the respective contrastText color */
