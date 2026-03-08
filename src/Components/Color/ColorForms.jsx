be2a4f9435de1bd78fbfded08134f574ca96c3d2
Saluomvienb <khamphasithivong.s@googlemail.com>
2026-03-08 16:06:46 +0100

    style changes for forms task2

--- src/Components/Color/ColorForms.jsx
+++ src/Components/Color/ColorForms.jsx
@@ -1,26 +1,36 @@
 import "./css/color-forms.css";
 import "./css/color-forms-role.css";
 
-export default function ColorForms () {
-    return <div className="color-forms">
-               
-                
-      <div className="color-forms-role">          
-                <label>Role</label>
-                <input type='text' id='role' placeholder="Pick your role here"></input>
+export default function ColorForms() {
+  return (
+    <div className="color-forms">
+      <div className="color-forms-role">
+        <label>Role</label>
+        <div>
+          <input
+            type="text"
+            id="role"
+            placeholder="Pick your role here"
+          ></input>
+        </div>
+      </div>
+      <div className="color-forms-role">
+        <label>Hex Color</label>
 
+        <div>
+          <input type="color" />
+          <input placeholder="Your Hex Color Code" />
+        </div>
+      </div>
+      <div className="color-forms-role">
+        <label>Contrast Text Color</label>
 
-    
-                <label>Hex Color</label>
-                <input type='color'></input><input placeholder="Your Hex Color Code"></input>
-                
-                <label>Contrast Text Color</label>
-                <input type='color'></input><input placeholder="Your Contrast Text Color"></input>
-
-                
-                </div>
-
-                <button>Add new color card</button> <button>Reset</button>
-
-           </div>
+        <div>
+          <input type="color"></input>
+          <input placeholder="Your Contrast Text Color"></input>
+        </div>
+      </div>
+      <button>Add new color card</button> <button>Reset</button>
+    </div>
+  );
 }
